dc52d81307677d7193556a2531d182101362358f
bopleromn <bopleromn@gmail.com>
2024-05-15 20:23:44 +0300

    commit

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from .server import Server
 
 if __name__ == '__main__':
     # set config 
-    server = Server('127.0.0.1', 8090)
+    server = Server('0.0.0.0', 8090)
 
     server.init_routes()
 
